51e7b60ab5f9a2f980bd394d359323fd0511dfd8
MsFreedom <1813700954@qq.com>
2022-03-31 16:50:25 +0800

    继承和多态

---
+++ 继承/继承/继承中的对象方式.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+public:
+	int m_A;
+protected:
+	int m_B;
+private:
+	int m_C; //私有成员只是被隐藏了，但是还是会继承下去
+};
+
+//公共继承
+class Son :public Base
+{
+public:
+	int m_D;
+};
+
+void test01()
+{
+	cout << "sizeof Son = " << sizeof(Son) << endl;
+}
+
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
