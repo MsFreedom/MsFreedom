c74b5e3b5a1ef8de9de9f033aca00f4657102f75
MsFreedom <1813700954@qq.com>
2022-04-01 18:39:05 +0800

    文件类型和读写方式

---
+++ 文件操作/文件操作/文本文件写文件.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+#include <fstream>
+
+void test01()
+{
+	ofstream ofs;
+	ofs.open("test.txt", ios::out);
+
+	ofs << "姓名：张三" << endl;
+	ofs << "性别：男" << endl;
+	ofs << "年龄：18" << endl;
+
+	ofs.close();
+}
+
+
+int main() {
+
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
---
+++ 文件操作/文件操作/文本文件读文件.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+#include <fstream>
+#include <string>
+void test01()
+{
+	ifstream ifs;
+	ifs.open("test.txt", ios::in);
+
+	if (!ifs.is_open())
+	{
+		cout << "文件打开失败" << endl;
+		return;
+	}
+
+	//第一种方式
+	//char buf[1024] = { 0 };
+	//while (ifs >> buf)
+	//{
+	//	cout << buf << endl;
+	//}
+
+	//第二种
+	//char buf[1024] = { 0 };
+	//while (ifs.getline(buf,sizeof(buf)))
+	//{
+	//	cout << buf << endl;
+	//}
+
+	//第三种
+	//string buf;
+	//while (getline(ifs, buf))
+	//{
+	//	cout << buf << endl;
+	//}
+
+	char c;
+	while ((c = ifs.get()) != EOF)
+	{
+		cout << c;
+	}
+
+	ifs.close();
+
+
+}
+
+int main() {
+
+	test01();
+
+
+	system("pause");
+
+	return 0;
+}
